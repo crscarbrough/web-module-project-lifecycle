014164ad1bdeea46e50937d2c9f4500394828e17
Claudia Scarbrough <rubyharrts@gmail.com>
2022-01-11 20:40:40 -0600

    ready to style

--- src/App.js
+++ src/App.js
@@ -1,43 +1,65 @@
 import React from "react";
 import User from "./components/User";
 import FollowerList from "./components/FollowerList";
+import Axios from "axios";
+import axios from "axios";
 
 class App extends React.Component {
   state = {
-    currentUser: "",
-    user: {
-      avatar_url: "https://avatars.githubusercontent.com/u/87742885?v=4",
-      html_url: "https://github.com/crscarbrough",
-      name: "Claudia Scarbrough",
-      login: "crscarbrough",
-      public_repos: 33,
-      followers: 6,
-    },
-    followers: [
-      {
-        login: "brandynwilliams",
-        avatar_url: "https://avatars.githubusercontent.com/u/66536877?v=4",
-        html_url: "https://github.com/brandynwilliams",
-      },
-      {
-        login: "brandynwilliams",
-        avatar_url: "https://avatars.githubusercontent.com/u/66536877?v=4",
-        html_url: "https://github.com/brandynwilliams",
-      },
-      {
-        login: "brandynwilliams",
-        avatar_url: "https://avatars.githubusercontent.com/u/66536877?v=4",
-        html_url: "https://github.com/brandynwilliams",
-      },
-    ],
+    currentUser: "crscarbrough",
+    user: {},
+    followers: [],
+  };
+
+  componentDidMount() {
+    axios
+      .get(`https://api.github.com/users/${this.state.currentUser}`)
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          user: resp.data,
+        });
+      });
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.user !== prevState.user) {
+      axios
+        .get(`https://api.github.com/users/${this.state.currentUser}/followers`)
+        .then((resp) => {
+          this.setState({
+            ...this.state,
+            followers: resp.data,
+          });
+        });
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      ...this.state,
+      currentUser: e.target.valie,
+    });
+  };
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    axios
+      .get(`https://api.github.com/users/${this.state.currentUser}`)
+      .then((resp) => {
+        this.setState({
+          ...this.state,
+          user: resp.data,
+        });
+      });
   };
 
   render() {
     return (
       <div>
         <h1>Github Card</h1>
-        <form>
-          <input placeholder="Github Handle" />
+        <form onSubmit={this.handleSubmit}>
+          <input placeholder="Github Handle" onChange={this.handleChange} />
           <button>Search</button>
         </form>
 
